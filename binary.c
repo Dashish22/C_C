859eec053b1443460e6092f6b923763e3681f210
Dashish22 <32478557+Dashish22@users.noreply.github.com>
2018-04-07 02:25:46 +0300

    Add files via upload
    19lsn

---
+++ binary.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	int binary ( int a, int mass[] )
+	{
+	
+	int low, high, middle;
+	low = 0;
+	high = 5;
+	while ( low <= high)
+	{
+		middle = (low + high) / 2;
+		if (a < mass[middle])
+		    high = middle - 1;
+		else if (a > mass[middle])
+		    low = middle + 1;
+		else
+		    return middle;
+	}
+	return -1;
+}
+}

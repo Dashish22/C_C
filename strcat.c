71c88c837a3ae4b0dcd94a32aa965baf77f2187d
Dashish22 <32478557+Dashish22@users.noreply.github.com>
2018-04-07 03:08:40 +0300

    Add files via upload
    strcat

---
+++ strcat.c
@@ -0,0 +1,12 @@
+﻿
+#include < stdio.h > #include < string.h >
+
+   int main(void) {
+      char second[] = " chocolate ";
+      char first[] = "kinder";
+
+      strcat(first, second);
+      printf("first: %s\n", first);
+
+      return 0;
+   }
